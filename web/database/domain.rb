70a2cf2ac2ea440e6d3e6fd08b3c54f962af69ad
Florian R. Hanke <florian.hanke@gmail.com>
2014-11-27 00:15:18 +0100

    Add all available tables.

--- web/database/domain.rb
+++ web/database/domain.rb
@@ -2,7 +2,15 @@
 #
 Domain = Flounder.domain(DB) do |dom|
   dom.entity(:schema_info, :schema_info, 'schema_info')
-  dom.entity(:schema_info_metrics, :schema_info_metrics, 'schema_info_metrics')
+  dom.entity(:schema_info_metrics, :schema_info_metric, 'schema_info_metrics')
+  
+  dom.entity(:tables, :table, 'tables')
+  
+  # Add all tables.
+  dom[:tables].where(table_schema: 'public').each do |table|
+    name = table.table_name
+    dom.entity(name.to_sym, name.to_sym, name)
+  end
 end
 
 # Define all tables as top-level methods on Domain.
