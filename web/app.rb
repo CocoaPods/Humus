eda9d12f015a108a5b319913211180b56f1c8a95
Florian R. Hanke <florian.hanke@gmail.com>
2014-11-27 00:15:38 +0100

    Provide counts. Add access token.

--- web/app.rb
+++ web/app.rb
@@ -7,12 +7,16 @@ require './web/database/db'
 require './web/database/domain'
 
 class Humus < Sinatra::Base
-  get '/api/v1/status' do
+  get "/api/v1/status/?#{ENV['ACCESS_TOKEN']}" do
     {
       versions: {
         schema_info: Domain.schema_info.first.version,
         schema_info_metrics: Domain.schema_info_metrics.first.version
-      }
+      },
+      counts: Domain.entities.inject({}) do |hash, entity|
+        hash[entity.name] = entity.count
+        hash
+      end
     }.to_json
   end
 end
