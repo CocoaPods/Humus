26385804b983c65a19d3c6b5391c25fd36084c16
Florian R. Hanke <florian.hanke@gmail.com>
2015-04-25 00:10:38 +0200

    Remove potentially dangerous code.
    Always run tests via `bundle exec rake`.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,3 @@
-ENV['RACK_ENV'] = 'test'
-
-# Load DB.
-#
-if ENV['LOAD_TEST_DB']
-  `pg_restore --verbose --clean --no-acl --no-owner -h localhost -d trunk_cocoapods_org_test spec/trunk.dump`
-end
-
 Bundler.require(:database, :test)
 
 require File.expand_path '../database/db', __FILE__
